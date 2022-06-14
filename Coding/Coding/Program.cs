0ac0e5b41f52aa7d50da76a63903906621b2f3f3
Aleksandrino04 <106624793+Aleksandrino04@users.noreply.github.com>
2022-06-14 23:23:02 +0300

    Add files via upload

---
+++ Coding/Coding/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Coding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceForDogs = 2.50;
+            double priceForCats = 4;
+            string pricePerPackageForDogs = Console.ReadLine();
+            double pricePerPackageForDogsDouble = double.Parse(pricePerPackageForDogs);
+
+            string pricePerPackageForCats = Console.ReadLine();
+            double pricePerPackageForCatsDouble = double.Parse(pricePerPackageForCats);
+
+            double totalPriceDogs = priceForDogs * pricePerPackageForDogsDouble;
+            double totalPriceCats = priceForCats * pricePerPackageForCatsDouble;
+            double totalPrice = totalPriceDogs + totalPriceCats;
+           
+            Console.WriteLine ($"{totalPrice} lv.");
+        }
+    }
+}
---
+++ Coding/Homework/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+
+            double bgn = usd * 1.79549;
+
+            Console.WriteLine(bgn);
+
+        }
+    }
+}
